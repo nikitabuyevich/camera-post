c480fc348ba9709711275f6f68fd0eee3ef72bcf
NikitaBuyevich <nikita.buyevich@gmail.com>
2016-03-12 18:23:36 -0600

    Initial Commit

---
+++ camera-post/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  camera-post
+//
+//  Created by Nikita Buyevich on 3/12/16.
+//  Copyright © 2016 Nikita Buyevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
