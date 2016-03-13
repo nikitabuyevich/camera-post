db39c3a4a0b85e4c937319c82b3fa3e332c1b281
NikitaBuyevich <nikita.buyevich@gmail.com>
2016-03-12 20:58:35 -0600

    init commit

---
+++ camera-post/DataService.swift
@@ -0,0 +1,67 @@
+//
+//  DataService.swift
+//  camera-post
+//
+//  Created by Nikita Buyevich on 3/12/16.
+//  Copyright © 2016 Nikita Buyevich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DataService {
+  static let instance = DataService()
+  
+  let KEY_POSTS = "posts"
+  private var _loadedPosts = [Post]()
+  
+  var loadedPosts: [Post] {
+    return _loadedPosts
+  }
+  
+  func savePosts() {
+    let postsData = NSKeyedArchiver.archivedDataWithRootObject(_loadedPosts)
+    NSUserDefaults.standardUserDefaults().setObject(postsData, forKey: KEY_POSTS)
+    NSUserDefaults.standardUserDefaults().synchronize()
+  }
+  
+  func loadPosts() {
+    if let postsData = NSUserDefaults.standardUserDefaults().objectForKey(KEY_POSTS) as? NSData {
+      if let postsArr = NSKeyedUnarchiver.unarchiveObjectWithData(postsData) as? [Post] {
+        _loadedPosts = postsArr
+      }
+    }
+  
+    NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "postsLoaded", object: nil))
+  }
+  
+  func saveImageAndCreatePath(image: UIImage) -> String {
+    let imgData = UIImageJPEGRepresentation(image, 100)
+    let imgPath = "image\(NSDate.timeIntervalSinceReferenceDate()).png"
+    let fullPath = documentsPathForFileName(imgPath)
+    imgData?.writeToFile(fullPath, atomically: true)
+    
+    return imgPath
+  }
+  
+  func imageForPath(path: String) -> UIImage? {
+    let fullPath = documentsPathForFileName(path)
+    let image = UIImage(named: fullPath)
+    
+    return image
+  }
+  
+  func addPost(post: Post) {
+    _loadedPosts.insert(post, atIndex: 0)
+    savePosts()
+    loadPosts()
+  }
+  
+  func documentsPathForFileName(name: String) -> String {
+    let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+    let fullPath = paths[0] as NSString
+    
+    return fullPath.stringByAppendingPathComponent(name)
+  }
+  
+}
---
+++ camera-post/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  camera-post
+//
+//  Created by Nikita Buyevich on 3/12/16.
+//  Copyright © 2016 Nikita Buyevich. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+  @IBOutlet weak var postImg: UIImageView!
+  @IBOutlet weak var titleLbl: UILabel!
+  @IBOutlet weak var descLbl: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+    postImg.layer.cornerRadius = postImg.frame.size.width / 2
+    postImg.clipsToBounds = true
+  }
+
+  func configureCell(post: Post) {
+    titleLbl.text = post.title
+    descLbl.text = post.postDesc
+    postImg.image = DataService.instance.imageForPath(post.imagePath)
+  }
+}
